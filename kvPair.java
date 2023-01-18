d2d9b52d2562655855624f2d24d584654902de1d
cyschlueter <91997798+cyschlueter@users.noreply.github.com>
2023-01-17 21:49:06 -0600

    Update kvPair.java
    Removing specific class references

--- kvPair.java
+++ kvPair.java
@@ -1,13 +1,7 @@
-// --== CS400 Project One File Header ==--
-// Name: Cy Schlueter
-// CSL Username: cschlueter (or it really might be cy I am not sure)
-// Email: cschlueter@wisc.edu
-// Lecture #: 002 @1:00pm
-// Notes to Grader: Thank you
+
 /**
  * This class allows me to store key-value pairs within a single object, so that I can access the key of a certain
  * value without looking in the hash table
- * @author Cy Schlueter
  *
  */
 public class kvPair<KeyType, ValueType> {
